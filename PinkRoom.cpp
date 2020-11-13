7284e0a0240a645703a1e327066072dbf36232b9
clairecashmore17 <73618817+clairecashmore17@users.noreply.github.com>
2020-11-13 13:41:35 -0700

    Update PinkRoom.cpp

--- PinkRoom.cpp
+++ PinkRoom.cpp
@@ -20,36 +20,87 @@ private:
 public:
 	int levelUp() { level++; };
 	int damage() { health = health - 10; }
+	void displayQuestion(int x);
 	void displayStory();
 };
-void playerStats::displayStory() {
-	
-	if (level == 0) {
-		ifstream fin("StoryLevel1.txt");
-		string line;
-		
-		fin >> line;
-		getline(fin, line);
-		cout << line << endl;
-		fin.close();
 
+void playerStats::displayQuestion(int level) {
+	switch (level) {
+		case 1:
+		/*Put Question answers here*/
+
+			break;
+
+		case 2:
+		/*Put Question answers here*/
+
+			break;
+		case 3:
+			/*Put Question answers here*/
+
+			break;
+		case 4:
+			/*Put Question answers here*/
+
+			break;
+		case 5:
+			/*Put Question answers here*/
+
+			break;
+		case 6:
+			/*Put Question answers here*/
+
+			break;
+		case 7:
+			/*Put Question answers here*/
+
+			break;
+		case 8:
+			/*Put Question answers here*/
+
+			break;
 	}
-	else if(level == 1) {
-	
+}
+
+
+
+
+void playerStats::displayStory() {
+	while (health != 0 && level != 8) {
+		if (level == 0) {
+			ifstream fin("StoryLevel1.txt");
+			string line;
+
+			fin >> line;
+			getline(fin, line);
+			cout << line << endl;
+			fin.close();
+
+		}
+		else if (level == 1) {
+
+		}
+		else if (level == 2) {
+
+		}
+		else if (level == 3) {
+
+		}
+		else {
+
+		}
+		displayQuestion(level);
 	}
-	else if (level == 2) {
-	
+	if (health == 0 && level < 8) {
+		cout << "You didn't answer the way I wanted you to! Feed them to the gators!" << endl;
 	}
-	else if (level == 3) {
-	
+	if (level == 8) {
+		cout << "You've answered correctly... I guess you aren't dumb after all. Go on, before I change my mind. " << endl;
 	}
-	else {
-	
-	}
-	
 }
 
 int main() {
 	playerStats p;
+	
 	p.displayStory();
 }
